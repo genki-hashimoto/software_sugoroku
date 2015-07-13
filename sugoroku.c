ab375004dc92e747f9aa7a17283aa3367e04f2a9
j11434 <j11434@sanma.3f.j.kisarazu.ac.jp>
2015-07-13 10:09:36 +0900

    a

--- sugoroku.c
+++ sugoroku.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 #define NAME_SIZE 16
 #define EVE_SIZE 64
+#define BOARD_STATUS_NUM 6
 
 #define PL_ACTIVE 0
 #define PL_GOAL 1
@@ -33,7 +37,35 @@ typedef struct player{
   struct board *next;
 }PLAYER;
 
+void init(PLAYER *player, BOARD * board);
+int load_board(BOARD *board);
+void init_player(PLAYER *player);
+
 int main(void)
 {
   return 0;
 }
+
+void init(PLAYER *player, BOARD *board)
+{
+  load_board(board);
+  init_player(player);
+}
+int load_board(BOARD *board)
+{
+  FILE *fp;
+  char *fname = "board.dat";
+  char tmp_str[EVE_SIZE];
+  int i;
+
+  fp = fopen( fname, "r" );
+  if( fp == NULL ){
+    printf( "can't open %s\n", fname );
+    return -1;
+  }
+
+  for(i = 0; ; i++) {
+  }
+}
+
+void init_player(PLAYER *player);
