841a67999350422b37719e54ad69125b9c31870b
Genki Hashimoto <knct.gen@gmail.com>
2015-09-07 09:49:42 +0900

    9/7

--- sugoroku.c
+++ sugoroku.c
@@ -18,6 +18,7 @@
 #define PL_GOAL 1
 
 #define INIT_POINT 10
+#define DICE_MAX 6
 
 #define EVE_NONE -1
 
@@ -28,8 +29,6 @@ typedef struct board{
   char about[EVE_SIZE];
   int real_p;
   int study_p;
-  int eve_sta;
-  int eve_num;
 }BOARD;
 
 typedef struct player{
@@ -44,6 +43,7 @@ typedef struct player{
   struct player *p_next;
 }PLAYER;
 
+int throw_dice(int max);
 void init(PLAYER *player, BOARD *board);
 int load_board(BOARD *board);
 void init_player(PLAYER *player);
@@ -51,6 +51,8 @@ PLAYER *decide_turn(PLAYER *player);
 void turn(PLAYER *player, BOARD *board);
 void calc_point(PLAYER *player, int real_p, int study_p);
 void print_board_info(BOARD info_board);
+void sort(int *array, int *p_num_array, int size);
+void result(PLAYER *player);
 
 int main(void)
 {
@@ -63,7 +65,14 @@ int main(void)
   init(player, board);
   p_head = decide_turn(player);
   turn(p_head, board);
+
+  result(player);
   return 0;
+}
+
+int throw_dice(int max)
+{
+  return rand() % max + 1;
 }
 
 void init(PLAYER *player, BOARD *board)
@@ -82,7 +91,7 @@ int load_board(BOARD *board)
   fp = fopen( fname, "r" );
   if( fp == NULL ){
     printf( "can't open %s\n", fname );
-    return -1;
+    return ERROR;
   }else{
     puts("get file!");
   }
@@ -98,7 +107,6 @@ int load_board(BOARD *board)
     fgets(tmp_str, EVE_SIZE, fp);
     board[i].study_p = atoi(tmp_str);
     fgets(tmp_str, EVE_SIZE,fp);
-    printf("id:%d\nname:%sabout:%sreal_p:%d\nstudy_p:%d\n\n",board[i].id, board[i].name, board[i].about, board[i].real_p, board[i].study_p);
   }
 
   return 0;
@@ -129,13 +137,14 @@ PLAYER *decide_turn(PLAYER *player)
   char dummy;
   int dice[PLAYER_NUM];
   int turn[PLAYER_NUM];
-  for(i = 0; i< PLAYER_NUM;i++) turn[i] = i;
   for(i = 0; i <PLAYER_NUM; i++) {
     printf("[%s] Please press enter",player[i].name);
     scanf("%c",&dummy);
-    dice[i] = rand() % 6 + 1;
+    dice[i] = throw_dice(DICE_MAX);
     printf("%d\n", dice[i]);
   }
+
+  sort(dice, turn, PLAYER_NUM);
   for (i = 0; i < PLAYER_NUM - 1; i++) {
     for (j = PLAYER_NUM - 1; j > i; j--) {
       if (dice[j - 1] < dice[j]) {
@@ -149,9 +158,6 @@ PLAYER *decide_turn(PLAYER *player)
     }
   }
 
-  for(i = 0; i < PLAYER_NUM; i++) {
-    printf("%d,%d\n",turn[i],dice[i]);
-  }
   for(i = 0; i < PLAYER_NUM; i++){
     if(i != PLAYER_NUM-1){
       player[turn[i]].p_next = &player[turn[i+1]];
@@ -173,7 +179,7 @@ void turn(PLAYER *player, BOARD *board)
       printf("sta-> リア充P:%d, 勉学P:%d\n", player->real_p, player->study_p);
       printf("[%s] Please type Enter",player->name);
       scanf("%c",&dummy);
-      player->dice = rand()%6+1;
+      player->dice = throw_dice(DICE_MAX);
       player->board_cnt += player->dice;
       if(player->board_cnt >= BOARD_NUM) {
         printf("[%s]ゴール！\n",player->name);
@@ -197,4 +203,54 @@ void print_board_info(BOARD info_board)
 {
   printf("%s\n", info_board.name);
   printf("%s\n", info_board.about);
+}
+
+void sort(int *array, int *p_num_array, int size)
+{
+  int i,j;
+  int tmp;
+
+  for(i = 0; i< size;i++) p_num_array[i] = i;
+  for (i = 0; i < PLAYER_NUM - 1; i++) {
+    for (j = PLAYER_NUM - 1; j > i; j--) {
+      if (array[j - 1] < array[j]) {
+        tmp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1]= tmp;
+        tmp = p_num_array[j];
+        p_num_array[j] = p_num_array[j-1];
+        p_num_array[j-1] = tmp;
+      }
+    }
+  }
+}
+
+void result(PLAYER *player) {
+  int i = 0;
+  int real_p[PLAYER_NUM], study_p[PLAYER_NUM],all_p[PLAYER_NUM];
+  int real_ranking[PLAYER_NUM], study_ranking[PLAYER_NUM], all_ranking[PLAYER_NUM];
+
+  for(i = 0; i < PLAYER_NUM; i++) {
+    real_p[i] = player[i].real_p;
+    study_p[i] = player[i].study_p;
+    all_p[i] = real_p[i] + study_p[i];
+  }
+  sort(real_p, real_ranking, PLAYER_NUM);
+  sort(study_p, study_ranking, PLAYER_NUM);
+  sort(all_p, all_ranking, PLAYER_NUM);
+
+  puts("リア充度ランキング");
+  for(i = 0; i < PLAYER_NUM; i++) {
+    printf("%d:%s, %dP\n",i+1, player[real_ranking[i]].name, player[real_ranking[i]].real_p);
+  }
+  puts("勉学励んだランキング");
+  for(i = 0; i < PLAYER_NUM; i++) {
+    printf("%d:%s, %dP\n",i+1, player[study_ranking[i]].name, player[study_ranking[i]].study_p);
+  }
+  puts("総合ランキング");
+  for(i = 0; i < PLAYER_NUM; i++) {
+    printf("%d:%s, %dP\n",i+1, player[all_ranking[i]].name, player[all_ranking[i]].real_p+player[all_ranking[i]].study_p);
+  }
+
+  puts("あなたの人生はどのような配分で進めていきますか？");
 }
